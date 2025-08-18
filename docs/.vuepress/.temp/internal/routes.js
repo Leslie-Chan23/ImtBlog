fcda7a308447981f2c9547ba5cf5a3d4f54d097b
Leslie <1440881060@qq.com>
2025-08-18 18:59:13 +0800

    backup at 25.8.18

--- docs/.vuepress/.temp/internal/routes.js
+++ docs/.vuepress/.temp/internal/routes.js
@@ -6,6 +6,8 @@ export const routes = Object.fromEntries([
   ["/articles/", { loader: () => import(/* webpackChunkName: "articles_index.html" */"C:/Users/14408/ImtBlog/docs/.vuepress/.temp/pages/articles/index.html.js"), meta: {"title":"文章列表"} }],
   ["/articles/movie/article1.html", { loader: () => import(/* webpackChunkName: "articles_movie_article1.html" */"C:/Users/14408/ImtBlog/docs/.vuepress/.temp/pages/articles/movie/article1.html.js"), meta: {"title":"A Brighter Summer Day"} }],
   ["/articles/movie/article10.html", { loader: () => import(/* webpackChunkName: "articles_movie_article10.html" */"C:/Users/14408/ImtBlog/docs/.vuepress/.temp/pages/articles/movie/article10.html.js"), meta: {"title":"Yi Yi"} }],
+  ["/articles/movie/article11.html", { loader: () => import(/* webpackChunkName: "articles_movie_article11.html" */"C:/Users/14408/ImtBlog/docs/.vuepress/.temp/pages/articles/movie/article11.html.js"), meta: {"title":"A Man Called Otto Decided To Die"} }],
+  ["/articles/movie/article12.html", { loader: () => import(/* webpackChunkName: "articles_movie_article12.html" */"C:/Users/14408/ImtBlog/docs/.vuepress/.temp/pages/articles/movie/article12.html.js"), meta: {"title":"nothing"} }],
   ["/articles/movie/article2.html", { loader: () => import(/* webpackChunkName: "articles_movie_article2.html" */"C:/Users/14408/ImtBlog/docs/.vuepress/.temp/pages/articles/movie/article2.html.js"), meta: {"title":"Blue Gate Crossing"} }],
   ["/articles/movie/article3.html", { loader: () => import(/* webpackChunkName: "articles_movie_article3.html" */"C:/Users/14408/ImtBlog/docs/.vuepress/.temp/pages/articles/movie/article3.html.js"), meta: {"title":"Cinema Paradiso"} }],
   ["/articles/movie/article4.html", { loader: () => import(/* webpackChunkName: "articles_movie_article4.html" */"C:/Users/14408/ImtBlog/docs/.vuepress/.temp/pages/articles/movie/article4.html.js"), meta: {"title":"Dead Poets Society"} }],
@@ -16,23 +18,4 @@ export const routes = Object.fromEntries([
   ["/articles/movie/article9.html", { loader: () => import(/* webpackChunkName: "articles_movie_article9.html" */"C:/Users/14408/ImtBlog/docs/.vuepress/.temp/pages/articles/movie/article9.html.js"), meta: {"title":"The Last Emperor"} }],
   ["/articles/movie/", { loader: () => import(/* webpackChunkName: "articles_movie_index.html" */"C:/Users/14408/ImtBlog/docs/.vuepress/.temp/pages/articles/movie/index.html.js"), meta: {"title":"movie"} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"C:/Users/14408/ImtBlog/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
-  ["/articles/movie/article11.html", { loader: () => import(/* webpackChunkName: "articles_movie_article11.html" */"C:/Users/14408/ImtBlog/docs/.vuepress/.temp/pages/articles/movie/article11.html.js"), meta: {"title":"nothing"} }],
-  ["/articles/movie/article12.html", { loader: () => import(/* webpackChunkName: "articles_movie_article12.html" */"C:/Users/14408/ImtBlog/docs/.vuepress/.temp/pages/articles/movie/article12.html.js"), meta: {"title":"nothing"} }],
 ]);
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updateRoutes) {
-    __VUE_HMR_RUNTIME__.updateRoutes(routes)
-  }
-  if (__VUE_HMR_RUNTIME__.updateRedirects) {
-    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ routes, redirects }) => {
-    __VUE_HMR_RUNTIME__.updateRoutes(routes)
-    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
-  })
-}
--- docs/.vuepress/.temp/pages/articles/movie/article9.html.js
+++ docs/.vuepress/.temp/pages/articles/movie/article9.html.js
@@ -1,16 +1,3 @@
 import comp from "C:/Users/14408/ImtBlog/docs/.vuepress/.temp/pages/articles/movie/article9.html.vue"
-const data = JSON.parse("{\"path\":\"/articles/movie/article9.html\",\"title\":\"The Last Emperor\",\"lang\":\"en-US\",\"frontmatter\":{},\"git\":{},\"filePathRelative\":\"articles/movie/article9.md\"}")
+const data = JSON.parse("{\"path\":\"/articles/movie/article9.html\",\"title\":\"The Last Emperor\",\"lang\":\"en-US\",\"frontmatter\":{},\"git\":{\"updatedTime\":1755253411000,\"contributors\":[{\"name\":\"Leslie\",\"username\":\"\",\"email\":\"1440881060@qq.com\",\"commits\":1}],\"changelog\":[{\"hash\":\"b7ee2f43bf808528ba5da83cb1734ed7e56759e7\",\"time\":1755253411000,\"email\":\"1440881060@qq.com\",\"author\":\"Leslie\",\"message\":\"Initial backup\"}]},\"filePathRelative\":\"articles/movie/article9.md\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
